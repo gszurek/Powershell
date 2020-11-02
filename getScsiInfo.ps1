38ab149e4c0a3f836e831d3fd6401067b546fefa
Szurkało <grzegorz.szurkalo@snpgroup.com>
2020-11-02 14:28:29 +0100

    Get count of  targets, devices, and paths per hba per host

---
+++ getScsiInfo.ps1
@@ -0,0 +1,16 @@
+#  Get count of  targets, devices, and paths per hba per host
+foreach($esx in Get-VMHost -Name "azan*"){
+    foreach($hba in (Get-VMHostHba -VMHost $esx -Type "FibreChannel")){
+        $target = ((Get-View $hba.VMhost).Config.StorageDevice.ScsiTopology.Adapter | where {$_.Adapter -eq $hba.Key}).Target
+        $luns = Get-ScsiLun -Hba $hba  -LunType "disk" -ErrorAction SilentlyContinue
+        $nrPaths = ($target | %{$_.Lun.Count} | Measure-Object -Sum).Sum
+        $props = [ordered]@{
+            VMHost = $esx.name
+            HBA = $hba.Name
+            Targets = $target.Count
+            Devices = $luns.Count
+            Paths = $nrPaths
+        }
+        New-Object PSObject -Property $props
+    }
+} 
